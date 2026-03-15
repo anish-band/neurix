25a124946173595e1b31d1d99f97733ced7c9b58
Anish Bandapelli <anish.bandapelli@gmail.com>
2026-03-15 14:31:14 -0400

    Configured clerk sign in and user button on dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,26 @@
+"use client";
+
+import Link from "next/link"
+import { useUser, SignInButton, UserButton } from "@clerk/nextjs";
+
 export default function Dashboard() {
-  return <h1>Dashboard</h1>
+  const {user} = useUser();
+
+  return (
+  <div>
+    <h1>Dashboard</h1>
+    <Link href="/">Home</Link>
+      <div>
+        {
+          !user ? (<SignInButton>
+            <button>Log In</button>
+          </SignInButton>) : (
+            <UserButton />
+          )
+        }
+      </div>
+  
+  </div>
+  
+  );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import { ClerkProvider } from '@clerk/nextjs'
 import "./globals.css";
 
+const PUBLISHABLE_KEY = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY;
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -28,7 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ClerkProvider>{children}</ClerkProvider>
+        <ClerkProvider publishableKey={PUBLISHABLE_KEY}>{children}</ClerkProvider>
       </body>
     </html>
   );
